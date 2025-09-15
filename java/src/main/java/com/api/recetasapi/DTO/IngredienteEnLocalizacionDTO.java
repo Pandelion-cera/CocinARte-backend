70b217c9529fc3640dc24c9dbe002dd69c361540
Juan <juanifabre1998@gmail.com>
2025-09-15 18:52:38 -0300

    ahora anda

--- java/src/main/java/com/api/recetasapi/DTO/IngredienteEnLocalizacionDTO.java
+++ java/src/main/java/com/api/recetasapi/DTO/IngredienteEnLocalizacionDTO.java
@@ -5,6 +5,15 @@ import lombok.Data;
 @Data
 public class IngredienteEnLocalizacionDTO {
 
+    public IngredienteEnLocalizacionDTO(Long localizacionId, String direccion, double latitud, double longitud, String ingrediente, int confiabilidad) {
+        this.localizacionId = localizacionId;
+        this.direccion = direccion;
+        this.latitud = latitud;
+        this.longitud = longitud;
+        this.ingrediente = ingrediente;
+        this.confiabilidad = confiabilidad;
+    }
+
     private Long localizacionId;
     private String direccion;
     private double latitud;
--- java/src/main/java/com/api/recetasapi/repository/IngredienteLocalizadoRepository.java
+++ java/src/main/java/com/api/recetasapi/repository/IngredienteLocalizadoRepository.java
@@ -5,38 +5,31 @@ import com.api.recetasapi.DTO.IngredienteLocalizadoDTO;
 import com.api.recetasapi.entities.IngredienteLocalizadoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface IngredienteLocalizadoRepository extends JpaRepository<IngredienteLocalizadoEntity, Integer> {
 
-    @Query(value = """
-    SELECT new tu.paquete.dto.LocalizacionConIngredienteDTO(
-      lc.id,
-      lc.direccion,
-      lc.latitud,
-      lc.longitud,
-      i.nombre,
-      il.confiabilidad
-    )
-    FROM IngredienteLocalizado il
-    JOIN il.localizaciones lc
-    JOIN il.ingredientes i
-    WHERE idingrediente = :idingrediente
-      AND (
-        6371 * acos(
-          cos(radians(:lat)) *
-          cos(radians(lc.latitud)) *
-          cos(radians(lc.longitud) - radians(:lng)) +
-          sin(radians(:lat)) *
-          sin(radians(lc.latitud))
-        )
-      ) <= :radioKm
-    ORDER BY il.confiabilidad DESC
-""")
-    List<IngredienteEnLocalizacionDTO> buscarLocalizacionesConIngredienteYConfiabilidad(int idingrediente, double lat, double lng, double radioKm);
+    @Query("SELECT new com.api.recetasapi.DTO.IngredienteEnLocalizacionDTO( " +
+            "lc.id, lc.direccion, lc.latitud, lc.longitud, i.nombre, il.confiabilidad) " +
+            "FROM IngredienteLocalizadoEntity il " +
+            "JOIN il.localizacion lc " +
+            "JOIN il.ingrediente i " +
+            "WHERE i.id = :idingrediente " +
+            "AND (6371 * acos(cos(radians(:lat)) * cos(radians(lc.latitud)) * " +
+            "cos(radians(lc.longitud) - radians(:lng)) + sin(radians(:lat)) * sin(radians(lc.latitud)))) <= :radioKm " +
+            "ORDER BY il.confiabilidad DESC")
+    List<IngredienteEnLocalizacionDTO> buscarLocalizacionesConIngredienteYConfiabilidad(
+            @Param("idingrediente") int idIngrediente,
+            @Param("lat") double lat,
+            @Param("lng") double lng,
+            @Param("radioKm") double radioKm
+    );
 
-    @Query("FROM IngredienteLocalizadoEntity il WHERE il.localizaciones.id = :idlocalizacion AND il.ingredientes.id = :idingrediente")
+    @Query("FROM IngredienteLocalizadoEntity il WHERE il.localizacion.id = :idlocalizacion AND il.ingrediente.id = :idingrediente")
     IngredienteLocalizadoEntity findByBoth(int idingrediente, int idlocalizacion);
 
 }
